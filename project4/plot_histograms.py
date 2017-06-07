fa266bd749537e94b5672a95dd49768d493a0f59
jonvegards <j.v.sparre@fys.uio.no>
2017-06-07 13:56:21 +0200

    updated with the other models trained

--- project4/plot_histograms.py
+++ project4/plot_histograms.py
@@ -3,6 +3,10 @@
 """
 Plotting data run through BDT made in BDT_refined.py
 
+!!! Be aware of the different arrays that should be given
+when using model with/without the transverse and
+invariant masses. !!!
+
 @author Jon Vegard Sparre
 May 2017
 """
@@ -31,13 +35,25 @@ df_zprime = pd.DataFrame(zprime)
 
 # Fetching values for chosen variables
 feature_list = ["lepton.pt()", "lepton.eta()", "lepton.phi()", "b_jet.pt()", "b_jet.eta()", "b_jet.phi()", "etmiss.et()", "etmiss.phi()", "TransMass", "InvMass"]
-egamma_features = df_egamma[feature_list].values
-zprime_features = df_zprime[feature_list].values
-muons_features  = df_muons[feature_list].values
+# Array with all values for plotting
+egamma_features_plot = df_egamma[feature_list].values
+zprime_features_plot = df_zprime[feature_list].values
+muons_features_plot  = df_muons[feature_list].values
+
+# Arrays without the tranverse and invariant masses for the models without these variables
+# egamma_features = df_egamma[feature_list[0:8]].values
+# zprime_features = df_zprime[feature_list[0:8]].values
+# muons_features  = df_muons[feature_list[0:8]].values
+
+# Use these if the model is trained with transverse and invariant masses
+egamma_features = egamma_features_plot
+zprime_features = zprime_features_plot
+muons_features  = muons_features_plot
 
 # Load model
 from sklearn.externals import joblib
-clf = joblib.load('AllData_depth1_n_estimators100_TransInv.pkl')
+# clf = joblib.load('AllData_depth1_n_estimators100.pkl')            # Model without tranverse and invariant masses
+clf = joblib.load('AllData_depth5_n_estimators100_TransInv.pkl') # Model with tranverse and invariant masses
 
 # Predict occurences of top quark in egamma data set
 pred_zprime = clf.predict(zprime_features)
@@ -79,11 +95,11 @@ m_bl = np.sqrt(170**2 - 80.385**2)
 
 # Invariant mass plots
 plot_histogram(4, r"Invariant mass of lepton and b-jet, Z' data set", r'$m_{b\ell}$ [GeV]',
-    r'No. of events without top', zprime_features[pred_0_zprime[0],9][:], bins_, "zprime_hist_0_inv.pdf",
+    r'No. of events without top', zprime_features_plot[pred_0_zprime[0],9][:], bins_, "zprime_hist_0_inv.pdf",
     xlim_inv, vertical=np.sqrt(170**2 - 80.385**2), verticallabel=r'$\sqrt{m_t^2 - m_W^2}$')
 
 plot_histogram(5, r"Invariant mass of lepton and b-jet, Z' data set", r'$m_{b\ell}$ [GeV]',
-    r'No. of events with top', zprime_features[pred_1_zprime[0],9][:], bins_, "zprime_hist_1_inv.pdf",
+    r'No. of events with top', zprime_features_plot[pred_1_zprime[0],9][:], bins_, "zprime_hist_1_inv.pdf",
     xlim_inv, vertical=np.sqrt(170**2 - 80.385**2), verticallabel=r'$\sqrt{m_t^2 - m_W^2}$')
 
 # # Transverse mass plots
@@ -116,14 +132,14 @@ plot_histogram(5, r"Invariant mass of lepton and b-jet, Z' data set", r'$m_{b\el
 #     xlim_trans, vertical=80.385, verticallabel=r'$m_W$')
 
 plt.figure(1)
-plt.hist(muons_features[pred_1_muons[0],9], bins=bins_, range=xlim_trans, label=r'$e \mu$ With $t$')
-plt.hist(egamma_features[pred_1_egamma[0],9], bins=bins_, range=xlim_trans, label=r'$e\gamma$ With $t$')
-plt.hist(muons_features[pred_0_muons[0],9], bins=bins_, range=xlim_trans, label=r'$e\mu$ Without $t$')
-plt.hist(egamma_features[pred_0_egamma[0],9], bins=bins_, range=xlim_trans, label=r'$e\gamma$ Without $t$')
+plt.hist(muons_features_plot[pred_1_muons[0],9], bins=bins_, range=xlim_trans, label=r'$\mu$ With $t$')
+plt.hist(egamma_features_plot[pred_1_egamma[0],9], bins=bins_, range=xlim_trans, label=r'$e\gamma$ With $t$')
+plt.hist(muons_features_plot[pred_0_muons[0],9], bins=bins_, range=xlim_trans, label=r'$\mu$ Without $t$')
+plt.hist(egamma_features_plot[pred_0_egamma[0],9], bins=bins_, range=xlim_trans, label=r'$e\gamma$ Without $t$')
 plt.axvline(m_bl, color='black', linestyle='dashed', label=r'$\sqrt{m_t^2 - m_W^2}$')
 plt.title("\n".join(textwrap.wrap(r"Invariant mass of lepton and $b$-jet, eGamma and eMuons data set", 50)))
 plt.xlim(xlim_trans)
 plt.xlabel(r'$m_{b\ell}$ [GeV]')
 plt.ylabel(r'No. of events')
 plt.legend()
-plt.savefig('egamma_emuons_invmass_01.pdf')
+plt.savefig('egamma_emuons_invmass_01_depth1.pdf')
